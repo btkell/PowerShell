5213eaa5106431f265d2b00728fba39b062439a9
btkell <btkellemans@gmail.com>
2025-01-03 22:47:11 -0500

    checkremoteservices

--- checkserviceonremotecomputer.ps1
+++ checkserviceonremotecomputer.ps1
@@ -1,4 +1,4 @@
-# Check remote computer services
+# Check remote computer service
 # Define the remote computer and the service name
 # please change the service name before running to the service you want on the remote machine
 $RemoteComputer = "omenserver"
