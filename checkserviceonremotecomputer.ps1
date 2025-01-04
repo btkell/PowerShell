38cdd5727bf49b92d70e40a2075287c7f029206d
btkell <btkellemans@gmail.com>
2025-01-03 22:20:25 -0500

    checkserviceremotecomputer

---
+++ checkserviceonremotecomputer.ps1
@@ -0,0 +1,10 @@
+# Define the remote computer and the service name
+# please change the service name before running to the service you want on the remote machine
+$RemoteComputer = "omenserver"
+$ServiceName = "spooler"
+$Credential = Get-Credential  # Prompts for username and password
+
+# Check the service status
+Invoke-Command -ComputerName $RemoteComputer -Credential $Credential -ScriptBlock {
+    Get-Service -Name $using:ServiceName
+}
